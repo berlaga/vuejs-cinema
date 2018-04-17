81f98690180ddd587041273be31461af8d1f5342
user <user@gmail.com>
2018-04-17 16:58:17 -0400

    added filter

--- src/main.js
+++ src/main.js
@@ -4,6 +4,25 @@ import genres from './util/genres';
 new Vue({
 
     el:'#app',
+    data:{
+        genre:[],
+        time: []
+    },
+    methods:{
+        checkFilter(category, title, checked){
+            if(checked)
+            {
+                this[category].push(title);
+            }
+            else{
+                let index = this[category].indexOf(title);
+                if(index > -1)
+                {
+                    this[category].splice(index, 1);
+                }
+            }
+        }
+    },
     components: {
         'movie-list': {
             template: `<div id='movie-list'>
@@ -35,9 +54,14 @@ new Vue({
             template: `<div id='movie-filter'>
                         <h2>Filter results</h2>
                         <div class="filter-group">  
-                            <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>
+                            <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
                         </div>
                         </div>`,
+            methods:{
+                checkFilter(category, title, checked){
+                    this.$emit('check-filter', category, title, checked);
+                }
+            },
             components: {
                 'check-filter':{
                     data(){
@@ -46,10 +70,17 @@ new Vue({
                         }
                     },
                     props:['title'],
-                    template: `<div v-bind:class="{'check-filter': true, 'active': checked }" v-on:click="checked = !checked">
+                    template: `<div v-bind:class="{'check-filter': true, 'active': checked }" v-on:click="checkFilter">
                                     <span class="checkbox"></span>
                                     <span class="check-filter-title">{{title}}</span>
-                                </div>`
+                                </div>`,
+                    methods:{
+                        checkFilter()
+                        {
+                            this.checked = !this.checked;                            
+                            this.$emit('check-filter', 'genre', this.title, this.checked);
+                        }
+                    }
                 }
             }
         }
