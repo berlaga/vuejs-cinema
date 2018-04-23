ab92d246a2c6f43f7f7d11c82555fe44333e39f1
user <user@gmail.com>
2018-04-23 16:07:41 -0400

    filtering for genre

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ new Vue({
     components: {
         'movie-list': {
             template: `<div id='movie-list'>
-                            <div v-for='movie in movies' class="movie">
+                            <div v-for='movie in filteredMovies' class="movie">
                             {{ movie.title }}
                             </div>
                         </div>`,
@@ -35,11 +35,29 @@ new Vue({
 
                 return {
                     movies: [
-                        {title: 'Pulp Fiction'},
-                        {title: 'Home Alone'},
-                        {title: 'Austin Powers'}
+                        {title: 'Pulp Fiction', genre: genres.CRIME},
+                        {title: 'Home Alone', genre: genres.COMEDY},
+                        {title: 'Austin Powers', genre: genres.COMEDY}
 
                     ]
+                }
+            },
+            props: ['genre', 'time'], 
+            methods:{
+                moviePassesGenreFilter(movie){
+                   if(!this.genre.length)
+                   {
+                        return true;
+                   }
+                   else
+                   {
+                        return this.genre.find(element => element === movie.genre)
+                   }
+                }
+            },
+            computed:{
+                filteredMovies(){
+                    return this.movies.filter(this.moviePassesGenreFilter);
                 }
             }
         },
