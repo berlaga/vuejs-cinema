028e2dbe58b326d38a9cbde1cabb1cc3ad0544c2
alex <alexey.geller@ontario.ca>
2019-01-03 15:52:06 -0500

    final version

---
+++ src/util/tooltip.js
@@ -0,0 +1,56 @@
+
+import {addClass, removeClass} from './helpers';
+
+let mouseOverHandler = function(event){
+    let span = event.target.parentNode.getElementsByTagName('SPAN')[0];
+    addClass(span, 'tooltip-show');
+};
+
+let mouseOutHandler = function(event){
+    let span = event.target.parentNode.getElementsByTagName('SPAN')[0];
+    removeClass(span, 'tooltip-show');
+};
+
+
+
+export default{
+
+    install(Vue){
+//custom directive 'tooltip'
+        Vue.directive('tooltip', {
+
+            bind(el, bindings){
+
+                let span = document.createElement('SPAN');
+                let text = document.createTextNode(`Seats available: ${bindings.value.seats}`);
+                span.appendChild(text);
+                addClass(span, 'tooltip');
+                el.appendChild(span);
+
+                let div = el.getElementsByTagName('DIV')[0];
+
+                div.addEventListener('mouseover', mouseOverHandler);
+                div.addEventListener('mouseout', mouseOutHandler);
+
+                //for mobile
+                div.addEventListener('touchstart', mouseOverHandler);
+                div.addEventListener('touchend', mouseOutHandler);
+
+            }, 
+            
+            unbind(el){
+
+                let div = el.getElementsByTagName('DIV')[0];
+
+                div.removeEventListener('mouseover', mouseOverHandler);
+                div.removeEventListener('mouseout', mouseOutHandler);
+
+                div.removeEventListener('touchstart', mouseOverHandler);
+                div.removeEventListener('touchend', mouseOutHandler);
+
+            }
+
+        });
+    }
+
+}
